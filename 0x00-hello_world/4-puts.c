5ff1641e4971d0c886e1e5143bcacc86d8488c5c
nickywanjau <nicolaswanjau@gmail.com>
2023-02-16 10:58:17 -0800

    program that prints a message followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main is the main part of the code
+ * return 0 if program is completed
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
